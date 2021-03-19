11e3356e18667b0b7ba2e11f5dac91f31c8e6982
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-03-19 09:58:35 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,10 +1,14 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
 def render_main():
     return render_template('home.html')
+
+def main():
+    with open('songs.json') as song_data:
+        songs = json.load(song_data)
 
 @app.route("/response")
 def render_response():
