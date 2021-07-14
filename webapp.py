ba49e0cbc7214e2062d1cf55be4d6c7dc9e7575d
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-07-14 00:07:41 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,8 +5,8 @@ import json
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
-def getHome():
-    return render_template('home.html')
+#def getHome():
+return render_template('home.html')
 
 @app.route("/response")
 def getResponse():
