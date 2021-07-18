13fb5fd1233b59e64ad3003d206e038eae449ee3
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-07-17 21:52:59 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -184,7 +184,7 @@ def getResponse():
         urls.append(str(song[taste]["song4"][platform]))#[3]
             
             
-    return render_template('response.html', response1 = replies[0], response2 = replies[1], response3 = replies[2], response4 = songRec1 + artistRec + songRec2 + artistRec2 + songRec3 + artistRec3 + songRec4 + artistRec4, songLink1 = urls[0], songLink2 = urls[1], songLink3 = urls[2], songLink4 = urls[3])
+    return render_template('response.html', response1 = replies[0], response2 = replies[1], response3 = replies[2], response4 = songRec1 + artistRec1 + songRec2 + artistRec2 + songRec3 + artistRec3 + songRec4 + artistRec4, songLink1 = urls[0], songLink2 = urls[1], songLink3 = urls[2], songLink4 = urls[3])
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
